c9a8b53ba7eaf4fe07469a5b42259da573969652
Mike Burnett <19mdmb@queensu.ca>
2024-11-18 13:58:47 -0800

    Add viewAllLAZ

---
+++ R/viewAllLAZ.R
@@ -0,0 +1,36 @@
+#' viewAllLAZ
+#' Displays all LAZ files  from a list, one by one.
+#'
+#' @param lazFile filename: A string representative of a singular LAZ/LAS file
+#'
+#' @return The RGL window displays a LAS point cloud
+#'
+#' @export viewAllLAZ
+#' @examples
+#' parentDir <- readClipboard() # Read the directory path of a folder containing several LAZ/LAS files
+#' setwd(parentDir)
+#' files <- list.files(pattern="*.laz")
+#' for(file in files){
+#'   viewAllLAZ(file)
+#' }
+viewAllLAZ <- function(lazFile){
+  temp_laz <- lidR::readLAS(lazFile)
+  if(is.empty(temp_laz)){ # In case there is no laz
+    cat(paste0("Skipping empty laz file: ",lazFile))
+    return()
+  }
+  # Access XYZ
+  x <- temp_laz@data$X
+  y <- temp_laz@data$Y
+  z <- temp_laz@data$Z
+  # Plot
+  rgl::open3d()
+  rgl::plot3d(x,y,z,col=terrain.colors(length(x)),size=3,main=lazFile,axes=F,xlab="",ylab="",zlab="")#,type="s")
+
+  # Wait for the user to close the rgl window before continuing to the next file
+  cat("Close the rgl window to continue to the next file.\n")
+  while (rgl::rgl.cur() > 0) {
+    Sys.sleep(0.1)  # Keep the window open and check periodically if it's closed
+  }
+}
+
