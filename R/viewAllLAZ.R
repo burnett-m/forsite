6c9f7ac6d214ef2900ced77788e12538938cff37
Mike Burnett <19mdmb@queensu.ca>
2025-04-02 09:38:55 -0700

    update viewAllLAZ

--- R/viewAllLAZ.R
+++ R/viewAllLAZ.R
@@ -1,7 +1,8 @@
 #' viewAllLAZ
 #' Displays all LAZ files  from a list, one by one.
 #'
-#' @param lazFile filename: A string representative of a singular LAZ/LAS file
+#' @param lazFile filenames: A string representative of a singular LAZ/LAS file
+#' @param plotView boolean: TRUE if you want a generalized view of the point cloud with file name, FALSE if you want a superior rendering with poor filename quality
 #'
 #' @return The RGL window displays a LAS point cloud
 #'
@@ -10,23 +11,32 @@
 #' parentDir <- readClipboard() # Read the directory path of a folder containing several LAZ/LAS files
 #' setwd(parentDir)
 #' files <- list.files(pattern="*.laz")
-#' for(file in files){
-#'   viewAllLAZ(file)
-#' }
-viewAllLAZ <- function(lazFile){
+#' for(file in files){viewAllLAS(file)}
+viewAllLAZ <- function(lazFile,plotView = FALSE){
   temp_laz <- lidR::readLAS(lazFile)
   if(lidR::is.empty(temp_laz)){ # In case there is no LAZ
     cat(paste0("Skipping empty laz file: ",lazFile))
     return()
   }
-  # Access XYZ
-  x <- temp_laz@data$X
-  y <- temp_laz@data$Y
-  z <- temp_laz@data$Z
-  # Plot
-  rgl::open3d()
-  rgl::plot3d(x,y,z,col=terrain.colors(length(z))[as.numeric(cut(z, breaks=length(z)))],size=3,main=lazFile,axes=F,xlab="",ylab="",zlab="")#,type="s")
+  if(isTRUE(plotView)){
+    # Access XYZ
+    x <- temp_laz@data$X
+    y <- temp_laz@data$Y
+    z <- temp_laz@data$Z
+    # Plot
+    rgl::open3d()
+    rgl::plot3d(x,y,z,col=terrain.colors(length(z))[as.numeric(cut(z, breaks=length(z)))],size=3,main=lazFile,axes=F,xlab="",ylab="",zlab="")#,type="s")
 
+  }
+  else{
+    plot(temp_laz,size=4)
+    rgl::bgplot3d({
+      par(bg='black')
+      plot.new()
+      abline(v = min(temp_laz@data$X),col='white')
+      title(main = lazFile,line=0.5,adj=1,col.main='white',cex.main=0.8,axis=TRUE)
+    })
+  }
   # Wait for the user to close the rgl window before continuing to the next file
   cat("Close the rgl window to continue to the next file.\n")
   while (rgl::rgl.cur() > 0) {
